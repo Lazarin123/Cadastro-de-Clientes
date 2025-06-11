8d219ca80b7f29860cdf26c182f49c2bb1487b75
Samuel Lazarin <samuel.lazarin12@gmail.com>
2025-06-11 20:30:42 -0300

    criação da função abertura

--- cadastro.c
+++ cadastro.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include "cadastro.h"
 
+void abertura() {
+    printf("*******************************************\n");
+    printf("**Seja bem-vindo ao cadastro de clientes!**\n");
+    printf("*******************************************\n");
+}
+
 int main () {
     opcao;
     abertura();
